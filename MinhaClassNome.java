2b96703020ba6c43abcffddc488574795836d016
joycecabral <joycecabral241@gmail.com>
2023-03-21 19:40:48 -0300

    MinhaClasseNome em java

---
+++ MinhaClassNome.java
@@ -0,0 +1,17 @@
+public class MinhaClassNome {
+
+public static void main (String[] args){
+	String primeiroNome = "Joyce";
+	String segundoNome = "Cabral";
+	
+	String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+	System.out.println(nomeCompleto);
+}
+
+	public static String nomeCompleto (String primeiroNome, String segundoNome){
+	return primeiroNome.concat(" ").concat(segundoNome);
+
+}
+
+}
