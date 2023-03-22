895163b2e84e53ecf14e0d7ac0cbff7f16fcedca
Joyce Cabral <79770441+joycecabral@users.noreply.github.com>
2023-03-21 21:01:50 -0300

    Update MinhaClassNome.java

--- MinhaClassNome.java
+++ MinhaClassNome.java
@@ -9,9 +9,9 @@ public static void main (String[] args){
 	System.out.println(nomeCompleto);
 }
 
-public static String nomeCompleto (String primeiroNome, String segundoNome){
+	public static String nomeCompleto (String primeiroNome, String segundoNome){
 	return primeiroNome.concat(" ").concat(segundoNome);
 
-}
+	}
 
 }
